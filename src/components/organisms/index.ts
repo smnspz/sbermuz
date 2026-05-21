0a44c25465407615dbeaee43fd25763d0ede5c25
Simone Rosani <rsn.smn@gmail.com>
2026-05-21 22:56:18 +0200

    Implement footer.

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,4 +1,5 @@
 export { default as EventCarousel } from './EventCarousel.astro';
+export { default as Footer } from './Footer.astro';
 export { default as Header } from './Header.astro';
 export { default as Hero } from './Hero.astro';
 export { default as Lightbox } from './Lightbox.astro';
