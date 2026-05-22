1331f58c5640599359b695f8aa1ee85445f1b3d3
Simone Rosani <rsn.smn@gmail.com>
2026-05-22 14:28:49 +0200

    Implement info section below the hero.

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -2,4 +2,5 @@ export { default as EventCarousel } from './EventCarousel.astro';
 export { default as Footer } from './Footer.astro';
 export { default as Header } from './Header.astro';
 export { default as Hero } from './Hero.astro';
+export { default as InfoBar } from './InfoBar.astro';
 export { default as Lightbox } from './Lightbox.astro';
