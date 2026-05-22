670d5f01854f3bdc327ba17d829e3b03897ade53
Simone Rosani <rsn.smn@gmail.com>
2026-05-22 14:50:05 +0200

    Implement filtering for archived events and added info box below hero.

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,3 @@
 export { default as Button } from './Button.astro';
 export { default as EventPhoto } from './EventPhoto.astro';
 export { default as Logo } from './Logo.astro';
-export { default as Marquee } from './Marquee.astro';
