dff54a24edc0968e509f7a9c35532a7659f29a55
Simone Rosani <rsn.smn@gmail.com>
2026-05-22 14:15:25 +0200

    Refactor button into its own atomic component.

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
+export { default as Button } from './Button.astro';
 export { default as EventPhoto } from './EventPhoto.astro';
 export { default as Logo } from './Logo.astro';
 export { default as Marquee } from './Marquee.astro';
