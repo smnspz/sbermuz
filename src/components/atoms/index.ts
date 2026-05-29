75cc67872df490be78ab006f799f2533d333173f
Simone Rosani <rsn.smn@gmail.com>
2026-05-29 12:22:49 +0200

    Implemented `BackToTop` component.

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
+export { default as BackToTop } from './BackToTop.astro';
 export { default as Button } from './Button.astro';
 export { default as EventPhoto } from './EventPhoto.astro';
 export { default as Logo } from './Logo.astro';
